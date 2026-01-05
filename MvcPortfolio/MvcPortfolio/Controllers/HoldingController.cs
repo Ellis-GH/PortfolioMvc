af07a8c9e2a94429b92b29d96a00e2da5f2d2533
Ellis <ellison.linsley@gmail.com>
2026-01-05 14:00:07 +0000

    Added functionality for creating a list of holdings, with total quantity and Remaining Invested Capital

---
+++ MvcPortfolio/MvcPortfolio/Controllers/HoldingController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MvcPortfolio.Data;
+using MvcPortfolio.Models;
+
+namespace MvcPortfolio.Controllers
+{
+    public class HoldingController : Controller
+    {
+        private readonly MvcPortfolioContext _context;
+
+        public HoldingController(MvcPortfolioContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Holding
+        public async Task<IActionResult> Index()
+        {
+            // var mvcPortfolioContext = _context.Transaction.Include(t => t.Ticker);
+            var holdings = await _context.Transaction
+                .Include(t => t.Ticker)
+                .GroupBy(t => t.Ticker)
+                .Select(g => new Holding
+                {
+                    Ticker = g.Key,
+                    Quantity = g.Sum(t => t.Buy ? t.Quantity : -t.Quantity),
+                    RIC = g.Sum(t => t.Buy ? t.Cost : -t.Cost)
+                })
+                .ToListAsync();
+
+            return View(holdings);
+            //return View(await mvcPortfolioContext.ToListAsync());
+        }
+    }
+}
---
+++ MvcPortfolio/MvcPortfolio/Models/Holding.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcPortfolio.Models
+{
+    public class Holding
+    {
+        [ForeignKey("Ticker")]
+        [StringLength(30)]
+        [Display(Name = "Ticker")]
+        public string TickerString { get; set; }
+        public virtual Ticker? Ticker { get; set; } //Navigation property
+
+        [NotMapped]
+        public decimal Quantity { get; set; }
+
+        [Display(Name = "Remaining Invested Capital")]
+        [NotMapped]
+        public decimal RIC { get; set; }
+
+        [NotMapped]
+        public decimal CurrentValue { get; set; }
+
+        [NotMapped]
+        public decimal TotalGain { get; set; }
+
+        [NotMapped]
+        public decimal TWR { get; set; }
+
+        [NotMapped]
+        public decimal XIRR { get; set; }
+    }
+}
